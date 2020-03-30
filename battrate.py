dbbb06b9c5e557d3e7ce50d24ed0d71867bfd841
dzg <zgilad@gmail.com>
2020-03-30 09:38:30 -0700

    Update battrate.py

--- battrate.py
+++ battrate.py
@@ -1,15 +1,14 @@
 import os, sqlite3
 hour = 3600
 scopes = ['day', 'week', 'month']
-battserial = os.popen("ioreg -rd1 -c AppleSmartBattery | awk -F\\\" '/BatterySerialNumber/ { print $(NF-1) }'").read().splitlines()[0]
+shellcmd = "ioreg -rd1 -c AppleSmartBattery | awk -F\\\" '/BatterySerialNumber/ { print $(NF-1) }'"
+battserial = os.popen(shellcmd).read().splitlines()[0]
 dbpath = os.path.expanduser('~') + '/Library/Application Support/iStat Menus/databases/iStatMenusStatus.db'
 c = sqlite3.connect(dbpath).cursor()
 
 print('(%/hr)  Max   Average')
 for scope in scopes:
-    c.execute(
-        'SELECT time,percentage from {}_batteryhistory where uuid="{}"'.format(
-            scope, battserial))
+    c.execute('SELECT time,percentage from {}_batteryhistory where uuid="{}"'.format(scope, battserial))
     data = c.fetchall()
     rates = []
     for row in range(0, len(data) - 1):
