4f62cbb692b1cc079de909c6dc1c819cb3f698a3
dzg <zgilad@gmail.com>
2020-04-07 11:57:28 -0700

    Update battrate.py

--- battrate.py
+++ battrate.py
@@ -1,5 +1,5 @@
 import os, sqlite3
-hour = 3600
+
 scopes = ['day', 'week', 'month']
 shellcmd = "ioreg -rd1 -c AppleSmartBattery | awk -F\\\" '/BatterySerialNumber/ { print $(NF-1) }'"
 battserial = os.popen(shellcmd).read().splitlines()[0]
@@ -8,12 +8,15 @@ c = sqlite3.connect(dbpath).cursor()
 
 print('(%/hr)  Max   Average')
 for scope in scopes:
-    c.execute('SELECT time,percentage from {}_batteryhistory where uuid="{}"'.format(scope, battserial))
-    data = c.fetchall()
-    rates = []
-    for row in range(0, len(data) - 1):
-        t = data[row + 1][0] - data[row][0]
-        p = data[row + 1][1] - data[row][1]
-        rates.append(p / (t / hour))
-    neg = list(filter(lambda x: x < 0, rates))
-    print("{:5} {:5.0f} {:9.0f}".format(scope.capitalize(), min(rates), sum(neg) / len(neg)))
+	c.execute('SELECT time,percentage from {}_batteryhistory where uuid="{}"'.format(scope, battserial))
+	data = c.fetchall()
+	rates = []
+	for row in range(0, len(data) - 1):
+		timedelta = data[row + 1][0] - data[row][0]
+		powerdelta = data[row + 1][1] - data[row][1]
+		rates.append(powerdelta / (timedelta / 3600))
+	neg = list(filter(lambda x: x < 0, rates))
+	if len(neg)>0:
+		print("{:5} {:5.0f} {:9.0f}".format(scope.capitalize(), min(rates), sum(neg) / len(neg)))
+	else:
+		print("{:5} No battery use".format(scope.capitalize()))
